b4dfaa6cef4472aee66f39bda54df977b9f814a6
Christopher Gervais <chris@ergonlogic.com>
2015-02-28 01:42:27 -0500

    Use Trusty for Vagrant tests, silence noisy curl download.

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,8 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 Vagrant.configure(2) do |config|
-  config.vm.box = "ubuntu/precise64"
+  #config.vm.box = "ubuntu/precise64"
+  config.vm.box = "ubuntu/trusty64"
 
   config.vm.provider "virtualbox" do |v|
     v.memory = 1024
--- tests/setup.sh
+++ tests/setup.sh
@@ -7,7 +7,7 @@
 #sudo rm -rf /var/lib/mysql
 #sudo rm -rf /root/.my.cnf
 
-curl https://raw.githubusercontent.com/GetValkyrie/ansible-bootstrap/master/install-ansible.sh | /bin/sh
+curl https://raw.githubusercontent.com/GetValkyrie/ansible-bootstrap/master/install-ansible.sh 2>/dev/null | /bin/sh
 ansible-galaxy install --ignore-errors http://github.com/getvalkyrie/ansible-role-aegir,,getvalkyrie.aegir
 target=/etc/ansible/roles/getvalkyrie.valkyrie
 if [ ! -e $target ]; then
